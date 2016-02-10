331cb3fa8a6fcc3568f9ece56ad4c479aab67add
fareedpatel <fareed_patel@hotmail.com>
2016-02-10 00:17:02 +0000

    Computer   computer chooses weapon
    Enter marketeers name
      submitting names
    
    playing the game
      When I submit Rock, I am told that I have won (FAILED - 1)
    
    Game
      stores computers choice of Rock, Paper, Scissors
      stores players choice of Rock, Paper, Scissors
      example of player #winning!
        player chooses rock, and computer chooses paper, player wins!
      example of player #losing!
        player chooses paper, and computer chooses scissors, player loses!
      example of player #drawing!
        player chooses paper, and computer chooses scissors, player loses!
    
    Player
      #name
        returns the name
      #choice_of_weapon
        allows player to choose Rock, Paper, Scissors

--- lib/game.rb
+++ lib/game.rb
@@ -24,4 +24,35 @@ WEAPON = [:rock, :paper, :scissors]
 	end
 
 
+
+	def win
+		if @player_choice = :paper && @computer_choice = :rock 
+			return "Player has won!" 
+		elsif @player_choice = :rock && @computer_choice = :scissors 
+			return "player has won!"
+		elsif @player_choice = :scissors && @computer_choice = :paper 
+			return "player has won!"
+		end
+	end
+
+	def lose 
+		if @player_choice = :rock && @computer_choice = :paper 
+			return "Player has lost!" 
+		elsif @player_choice = :scissors && @computer_choice = :rock 
+			return "player has lost!"
+		elsif @player_choice = :scissors && @computer_choice = :paper 
+			return "player has lost!"
+		end
+	end
+
+	def draw
+		if @player_choice = :rock && @computer_choice = :rock 
+			return "Player has lost!" 
+		elsif @player_choice = :scissors && @computer_choice = :scissors 
+			return "player has lost!"
+		elsif @player_choice = :paper && @computer_choice = :paper 
+			return "player has lost!"
+		end
+	end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,14 +4,52 @@ describe Game do
 	subject(:game) { described_class.new(computer, player)}
 	let(:computer) {double :computers_choice}
 	let(:player)   {double :weapon}
+	let(:player)	{double :win}
+	let(:player)  {double :lose}
+	let(:player)  {double :draw}
 
-	it 'stores computers choice of Rock, Paper, Scissors' do
-		allow(computer).to receive(:choose_weapon).and_return :rock
-		expect(game.computers_choice).to eq :rock
+
+
+		it 'stores computers choice of Rock, Paper, Scissors' do
+			allow(computer).to receive(:choose_weapon).and_return :rock
+			expect(game.computers_choice).to eq :rock
+		end
+
+		it 'stores players choice of Rock, Paper, Scissors' do 
+			allow(player).to receive(:weapon).and_return :paper
+			expect(game.players_choice).to eq :paper
+		end
+
+	context 'example of player #winning!' do
+
+		it 'player chooses rock, and computer chooses paper, player wins!' do
+			allow(computer).to receive(:choose_weapon).and_return :rock
+			allow(player).to receive(:weapon).and_return :paper
+			allow(player).to receive(:win).and_return :win
+			expect(game.win).to eq 'Player has won!'
+		end
+
 	end
 
-	it 'stores players choice of Rock, Paper, Scissors' do 
-		allow(player).to receive(:weapon).and_return :paper
-		expect(game.players_choice).to eq :paper
+	context 'example of player #losing!' do
+
+		it 'player chooses paper, and computer chooses scissors, player loses!' do
+			allow(computer).to receive(:choose_weapon).and_return :scissors
+			allow(player).to receive(:weapon).and_return :paper
+			allow(player).to receive(:lose).and_return :lose
+			expect(game.lose).to eq 'Player has lost!'
+		end
 	end
+
+	context 'example of player #drawing!' do
+
+		it 'player chooses paper, and computer chooses scissors, player loses!' do
+			allow(computer).to receive(:choose_weapon).and_return :scissors
+			allow(player).to receive(:weapon).and_return :scissors
+			allow(player).to receive(:draw).and_return :draw
+			expect(game.draw).to eq 'Player has lost!'
+		end
+
+	end
+
 end
